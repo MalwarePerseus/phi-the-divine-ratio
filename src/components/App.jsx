e6040ed28137d83aeef1361ec3f96b6d0a3dc10c
Deepak Singh <malwareperseus@gmail.com>
2020-08-02 15:44:05 +0530

    Added Navbar using  Bootstrap 5 alpha.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Switch, Route, Redirect} from 'react-router-dom';
-
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './App.css';
 
 import Home from './Home';
 import TIY from './Tiy';
 import Illustrations from './Illustrations';
 import About from './About';
+import Navbar from './Navbar';
 
 const App = () => {
 	return(
 		<>
+			<Navbar />
 			<Switch>
 				<Route exact path='/' component={Home} />
 				<Route exact path='/about' component={About} />
