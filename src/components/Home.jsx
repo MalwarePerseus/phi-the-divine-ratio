e5dc3d7c70ed4163d69bd5600cd6c1dcf15b3a97
Deepak Singh <malwareperseus@gmail.com>
2020-08-03 10:04:54 +0530

    Header Image and minor Changes.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
 	return (
 		<>
-			<h1>Welcome to the Home Page</h1>
+			<section id='header' className='mx-auto pushup'>
+				<img src='images/illusion.gif' alt="" className="pushleft" width="1000px" height="600px" /> 
+			</section>
+			<h1 className="pushup hede">Welcome to the Home Page</h1>
 		</>
 	);
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 			<div className="container-fluif nav_bg">
 				<div className="row">
 					<div className="col-10 mx-auto">
-						<nav className="navbar navbar-expand-lg navbar-light bg-light">
+						<nav className="navbar navbar-expand-lg navbar-dark bg-light">
 							<div className="container-fluid">
 							    <NavLink className="navbar-brand" to="/">The Golden Ratio</NavLink>
 							    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
