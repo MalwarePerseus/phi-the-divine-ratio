a69ba780d79c5a62a742080c9af1c773ff7bb9eb
Deepak Singh <malwareperseus@gmail.com>
2020-08-06 23:48:35 +0530

    Added and implemented the Paricles Component and Some New Fonts.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 					<div className="col-10 mx-auto">
 						<nav className="navbar navbar-expand-lg navbar-dark bg-light">
 							<div className="container-fluid">
-							    <NavLink className="navbar-brand" to="/">The Golden Ratio</NavLink>
+							    <NavLink className="navbar-brand" to="/">φ The Golden Ratio</NavLink>
 							    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							    	<span className="navbar-toggler-icon"></span>
 							    </button>
---
+++ src/components/Particless.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+const Particless = () => {
+    return(            <div className="pushup2"><Particles params={{
+                "interactivity": {
+                    "detect_on": "window",
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "onclick": {
+                            "enable": false,
+                            "mode": "bubble"
+                        },
+                        "resize": true
+                    },
+                    "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                                "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 400,
+                            "size": 40,
+                            "duration": 2,
+                            "opacity": 8,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 100,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            "particles_nb": 4
+                        },
+                        "remove": {
+                            "particles_nb": 2
+                        }
+                    }
+                }, 
+                "particles": { 
+                	"number": { 
+                		"value": 12,
+                		 "density": { 
+                		 	"enable": true,
+                		 	"value_area": 100,
+                		 	}
+                		},
+                	
+                    "size": {
+                        "value": 2.0,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                        }
+                    },
+                    
+                    "move": {
+                        "enable": true,
+                        "speed": 4,
+                        
+                    }
+
+               	},
+               	"retina_detect": true
+
+
+                }}  /> 
+			</div>
+        )}
+
+export default Particless;
