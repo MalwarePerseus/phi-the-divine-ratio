0d7aeb1f57f506b95c0c52680688af8c4e5bd489
Deepak Singh <malwareperseus@gmail.com>
2020-08-30 01:16:06 +0530

    Minor Changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
         <div className="col-md-4 col-10 mx-auto">
             <div className="card">
                 <img src={props.imgsrc}  className="img-responsive shadow-lg p-2" alt={props.imgsrc} />
-                {console.log(props.imgsrc)}
+               
             </div>
         </div>
     );
